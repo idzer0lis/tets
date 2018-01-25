ef411e044aa0887f57312fa85069a5cb17330ca8
Mihaila Botez Andrei <idzer0lis@gmail.com>
2018-01-25 22:42:32 +0200

    Update login.spec.js

--- frontend-web-app/test/unit/specs/login.spec.js
+++ frontend-web-app/test/unit/specs/login.spec.js
@@ -10,16 +10,21 @@ import Actions from '@/components/../store/actions';
 const localVue = createLocalVue();
 localVue.use(Vuex);
 localVue.use(VueRouter);
-
+localVue.use(Notify);
 describe('Login.vue', () => {
   let actions, store, mutations, getters;
+  let submitLoginStub = function() {
 
+  };
 
   beforeEach(() => {
     actions = {
+      getSession: function () {
+
+      },
       setCaptcha: function () {
         //
-      }
+      },
     };
     mutations = {
       SET_FLASH_MESSAGES: function () {
@@ -27,7 +32,9 @@ describe('Login.vue', () => {
       }
     };
     store = new Vuex.Store({
-      state: {},
+      state: {
+        userCaptcha: 'string for simulating captcha pass'
+      },
       actions,
       mutations
     })
@@ -35,19 +42,40 @@ describe('Login.vue', () => {
 
   it('should render a form with inputs and button', () => {
 
-    const wrapper = mount(Login, {store, localVue });
+    const wrapper = mount(Login, {store, localVue, submitLogin: submitLoginStub });
     const form = wrapper.find('form');
     const inputs = form.findAll('b-input').wrappers;
     const button = form.find('button');
 
-    expect(form).to.exist;
-    expect(button).to.exist;
+    expect(form).to.exists;
+    expect(button).exists;
 
     // check the existance of both inputs and test their types
     expect(inputs.length).to.equal(2);
     expect(inputs[0].html()).contains('email');
     expect(inputs[1].html()).to.contain('password');
 
+  });
 
+  describe('should make the login request when pressing the submit button', ()=> {
+    it('should only make the request when both inputs are filled', ()=> {
+      const wrapper = mount(Login, {store, localVue });
+      const inputs = wrapper.findAll('b-input').wrappers;
+      wrapper.setProps({
+        email: 'a@a.ro',
+        password: 'passworD!'
+      });
+
+      sinon.spy(wrapper.vm, "submitLogin");
+      wrapper.find('button').trigger('click');
+      expect(wrapper.vm.submitLogin).to.have.been.called;
+
+    });
+    it('should have validation', ()=> {
+      const wrapper = mount(Login, {store, localVue });
+      sinon.spy(wrapper.vm, "submitLogin");
+      wrapper.find('button').trigger('click');
+      expect(wrapper.vm.submitLogin).to.throw();
+    });
   });
 });
